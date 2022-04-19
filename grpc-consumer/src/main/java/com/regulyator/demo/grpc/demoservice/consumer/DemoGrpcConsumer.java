d1d07772217868bd66693577ea798ca6e4fbac51
regulyator <regulyator777@gmail.com>
2022-04-20 02:30:54 +0300

    in progress

--- grpc-consumer/src/main/java/com/regulyator/demo/grpc/demoservice/consumer/DemoGrpcConsumer.java
+++ grpc-consumer/src/main/java/com/regulyator/demo/grpc/demoservice/consumer/DemoGrpcConsumer.java
@@ -1,14 +1,21 @@
 package com.regulyator.demo.grpc.demoservice.consumer;
 
-import com.regulyator.demo.grpc.demoservice.DemoRequest;
-import com.regulyator.demo.grpc.demoservice.DemoResponse;
-import com.regulyator.demo.grpc.demoservice.DemoServiceGrpc;
+import com.regulyator.demo.grpc.DemoRequest;
+import com.regulyator.demo.grpc.DemoResponse;
+import com.regulyator.demo.grpc.DemoServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
-
+@GrpcService
 public class DemoGrpcConsumer extends DemoServiceGrpc.DemoServiceImplBase {
+
     @Override
-    public StreamObserver<DemoRequest> hello(StreamObserver<DemoResponse> responseObserver) {
-        return super.hello(responseObserver);
+    public void hello(DemoRequest request, StreamObserver<DemoResponse> responseObserver) {
+        DemoResponse response = DemoResponse.newBuilder()
+                .setCounterResponse(request.getCounterRequest()+1)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
+
 }
